4a2f53a47a2f9857e24d3b7034a3aea6de5f599d
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-11-10 14:30:21 -0300

    Classe Reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -2,27 +2,23 @@ namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
     {
-        public List<Pessoa> Hospedes { get; set; }
-        public Suite Suite { get; set; }
-        public int DiasReservados { get; set; }
-
         public Reserva() { }
 
         public Reserva(int diasReservados)
         {
             DiasReservados = diasReservados;
         }
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            if (Suite.Capacidade >= hospedes.Count) {
+            if (Suite.Capacidade >= hospedes.Count)
                 Hospedes = hospedes;
-                
-            } else {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                throw new Exception("Capacidade menor do que o número de hóspedes");
-            }
+            else
+                throw new Exception(" Número de hóspedes maior do que a capacidade da Suíte. ");
         }
 
         public void CadastrarSuite(Suite suite)
@@ -32,29 +28,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
             decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            if (DiasReservados >= 10) {
+            if (DiasReservados >= 10)
                 valor -= Decimal.Divide(Decimal.Multiply(valor, 10), 100);
-            }
 
             return valor;
-        }
-    }
-
-    class Excecao : Exception
-    {
-        public Excecao(string mensagem) : base(mensagem)
-        {
-            // Construtor que chama o construtor da classe genérica
         }
     }
 }
